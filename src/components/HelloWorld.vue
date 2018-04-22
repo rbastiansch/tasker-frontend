570f9c6ea940f4b6691d2a4a8b33d82418c7a379
Rafael Bastiansch <rafaelbastiansch@gmail.com>
2018-04-22 13:21:28 -0300

    Initial setup

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    {{msg}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+    };
+  },
+};
+</script>
